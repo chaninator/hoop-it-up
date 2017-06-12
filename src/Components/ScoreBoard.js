8595b0f9ada39634dcd384695431fce263a6f3f9
Salman Rana <salman.r.rana@gmail.com>
2017-06-12 17:23:03 -0500

    about to merge

--- src/Components/ScoreBoard.js
+++ src/Components/ScoreBoard.js
@@ -4,6 +4,22 @@ import Away from './Away';
 import './scoreboard.css';
 
 class ScoreBoard extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      scores: 0
+    }
+  }
+
+  resetScore(){
+    let scores = this.state.scores
+    console.log('score', this.state.scores);
+    scores = 0
+    console.log('this.state.scores', this.state.scores);
+
+    this.setState({scores: scores})
+  }
 
   render() {
     return (
@@ -22,7 +38,7 @@ class ScoreBoard extends Component {
         <div className="net9"></div>
         <Home />
         <Away />
-        <button className="reset">NEW GAME</button>
+        <a className="reset" onClick={() => this.resetScore()}>NEW GAME</a>
       </div>
     );
   }
