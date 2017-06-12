6a20c73d5a2a92a42170811bd21f5e3877b5e499
Salman Rana <salman.r.rana@gmail.com>
2017-06-12 13:50:57 -0500

    merge

--- src/Components/Away.js
+++ src/Components/Away.js
@@ -7,9 +7,15 @@ class Away extends Component {
   render() {
     return (
         <div>
-          <h4 class="away">AWAY</h4>
-          <div class="div_line_away"></div>
-          <div class="score_container_away">30</div>
+          <h4 className="away">AWAY</h4>
+          <div className="div_line_away"></div>
+          <div className="score_container_away">30</div>
+
+          <div className="add_button btn1">+1</div>
+          <div className="add_button btn2">+2</div>
+          <div className="add_button btn3">+3</div>
+
+          <div className="minus_button btn-subtract2">-1</div>
         </div>
     );
   }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,9 +7,16 @@ class Home extends Component {
   render() {
     return (
         <div>
-          <h4 class="home">HOME</h4>
-          <div class="div_line_home"></div>
-          <div class="score_container_home">24</div>
+          <h4 className="home">HOME</h4>
+          <div className="div_line_home"></div>
+          <div className="score_container_home">24</div>
+
+          <div className="add_button btn4">+1</div>
+          <div className="add_button btn5">+2</div>
+          <div className="add_button btn6">+3</div>
+
+          <div className="minus_button btn-subtract1">-1</div>
+
         </div>
     );
   }
