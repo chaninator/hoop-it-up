85ce98d9498874fab63deb001cec042e162e940f
Andrew Chan <accsuperstar@gmail.com>
2017-06-12 13:26:20 -0500

    create components

---
+++ src/Components/ScoreBoard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './scoreboard.css';
+
+class ScoreBoard extends Component {
+
+
+  render() {
+    return (
+      <div className="App"></div>
+    );
+  }
+}
+
+export default ScoreBoard;
